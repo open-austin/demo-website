01e2234d50f7d00efc3e592299111ea1a673aaf5
Chip Rosenthal <chip@unicom.com>
2014-06-15 09:25:48 -0500

    Updated contact us block on OpenData page. Added @links[:CONTACT_FORM] template parameter, and new defs.erb file.

--- src/mkpage.rb
+++ src/mkpage.rb
@@ -3,15 +3,21 @@ require 'erb'
 
 USAGE = "usage: #{$0} page.rhtml"
 LAYOUT = File.dirname($0) + "/layout.rhtml"
+DEFS = File.dirname($0) + "/defs.erb"
 
 class Page
   attr_accessor :title
   attr_accessor :path
   attr_accessor :content
+  attr_accessor :links
 
   def initialize(fname)
     @layout_src = open(LAYOUT) {|fp| fp.read}
     @content_src = open(fname) {|fp| fp.read}
+    @links = {}
+
+    defs_src = open(DEFS) {|fp| fp.read}
+    ERB.new(defs_src).result(binding)
   end
 
   def menu_item(target, description)
